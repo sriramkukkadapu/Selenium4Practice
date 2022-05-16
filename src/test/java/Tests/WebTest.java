604997233728725ccbeb4a549fafefced046ea5c
sriramkukkadapu@gmail.com <sriramkukkadapu@gmail.com>
2022-05-16 02:07:17 -0500

    Sriram|May 16|github actions commit

---
+++ src/test/java/Tests/WebTest.java
@@ -0,0 +1,26 @@
+package Tests;
+
+import lombok.SneakyThrows;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class WebTest {
+
+    @SneakyThrows
+    @Test
+    public void testGoogleSearchUsingSelenoid() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("browserName", "chrome");
+        capabilities.setCapability("enableVNC", false);
+        capabilities.setCapability("enableVideo", false);
+        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),capabilities);
+        driver.get("https://google.co.in");
+        Assert.assertEquals(driver.getTitle(), "Google");
+    }
+}
